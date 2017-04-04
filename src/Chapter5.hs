90f8fb4230d7fe444b337d269f943d0e01dd890e
futtetennista <futtetennista@gmail.com>
2017-04-04 20:01:26 +0200

    [WIP] Chapter 5

--- src/Chapter5.hs
+++ src/Chapter5.hs
@@ -15,6 +15,11 @@ binom n k
     binom (n - 1) k + binom (n - 1) (k - 1)
 
 
+binomSum :: Int -> Int
+binomSum n =
+  sum [binom n k | k <- [0..n]]
+
+
 pascal :: Int -> [[Int]]
 pascal x =
   [map (uncurry binom) xs | xs <- xss]
@@ -75,3 +80,105 @@ printPascal x =
     digits :: Integral a => Int -> a
     digits n =
       floor $ logBase 10.0 (fromIntegral n) + 1
+
+
+(!) :: [a] -> Int -> a
+[]!_ =
+  error "empty list"
+(x:xs)!i
+  | i == 0 =
+    x
+  | otherwise =
+    xs!(i - 1)
+
+
+takewhile :: (a -> Bool) -> [a] -> [a]
+takewhile _ [] =
+  []
+takewhile p (x:xs)
+  | p x =
+    x : takewhile p xs
+  | otherwise =
+    []
+
+
+dropwhile :: (a -> Bool) -> [a] -> [a]
+dropwhile _ [] =
+  []
+dropwhile p ys@(x:xs)
+  | p x =
+    dropwhile p xs
+  | otherwise =
+    ys
+
+
+inits :: [a] -> [[a]]
+inits [] =
+  [[]]
+inits (x:xs) =
+  [[]] ++ map (x:) (inits xs)
+
+
+subs :: [a] -> [[a]]
+subs [] =
+  [[]]
+subs (x:xs) =
+  subs' ++ map (x:) subs'
+  where
+    subs' =
+      subs xs
+
+
+intersperse :: a -> [a] -> [[a]]
+intersperse x [] =
+  [[x]]
+intersperse x (y:ys) =
+  [x:y:ys] ++ map (y:) (intersperse x ys)
+
+
+perms :: [a] -> [[a]]
+perms [] =
+  [[]]
+perms (x:xs) =
+  concat . map (intersperse x) $ perms xs
+
+
+perms' :: [a] -> [[a]]
+perms' [] =
+  [[]]
+perms' (x:xs) =
+  [zs | ys <- perms xs, zs <- intersperse x ys]
+
+
+-- 5.6.1
+-- length segs = sum [1..length xs] + 1
+segs :: [a] -> [[a]]
+segs [] =
+  [[]]
+segs (x:xs) =
+  segs xs ++ map (x:) (inits xs)
+
+segs' :: [a] -> [[a]]
+segs' xs =
+  segstr xs [[]]
+  -- segstr xs []
+  where
+    segstr [] acc =
+      acc
+      -- [[]] ++ acc
+    segstr (y:ys) acc =
+      segstr ys (map (y:) (inits ys) ++ acc)
+
+
+-- 5.6.2
+-- length (choose k xs) == binom (length xs) k
+choose :: Int -> [a] -> [[a]]
+choose _ [] =
+  [[]]
+choose 0 _ =
+  [[]]
+choose k xs@(y:ys)
+  | length xs == k =
+    [xs]
+  | otherwise =
+    choose k ys ++ map (y:) (choose (k - 1) ys)
