501fd758deebc0f9a06cf59ab14c884d972feafd
futtetennista <futtetennista@gmail.com>
2017-06-21 16:51:10 +0200

    PiH chapter 11

--- src/pih/Exercises.hs
+++ src/pih/Exercises.hs
@@ -4,6 +4,8 @@ where
 
 import System.IO (hSetEcho, stdin)
 import Countdown
+import TicTacToe
+import System.Random (randomRIO)
 
 
 -- Ex. 9.2
@@ -25,17 +27,10 @@ isChoice (x:xs) ys =
     isChoiceHelp (z:zs) ws True =
       isChoiceHelp zs (minusElem z ws) (elem z ws)
 
-    -- defenestrate _ [] =
-    --   []
-    -- defenestrate y (z:zs)
-    --   | y == z =
-    --     zs
-    --   | otherwise =
-    --     z : defenestrate y zs
-
     -- Time complexity: O(Z) where Z = length (z:zs)
-    minusElem xs ys =
-      minusElemHelp xs ys []
+    minusElem :: Eq a => a -> [a] -> [a]
+    minusElem x ys =
+      minusElemHelp x ys []
       where
         minusElemHelp _ [] acc =
           acc
@@ -43,7 +38,8 @@ isChoice (x:xs) ys =
           | y == z =
             acc ++ zs
           | otherwise =
-            minusElem (acc ++ [z]) y zs
+            minusElemHelp y zs (acc ++ [z])
+
 
 -- Ex. 9.4
 verifyExprCount :: Either Int Int
@@ -166,3 +162,120 @@ getLine' =
       []
     safeTail (_:xs) =
       xs
+
+
+-- Ex. 11.1
+verifyGameTreeCount :: Either String Int
+verifyGameTreeCount =
+  depthE >> nodeCountE
+  where
+    depthE =
+      if d == 9 then Right d else Left ("depth=" ++ show d) where d = depth gtree
+
+    nodeCountE =
+      if nodeCount == 549946 then Right nodeCount else Left ("#nodes=" ++ show nodeCount)
+
+    gtree =
+      gametree empty X
+
+    nodes :: GTree a -> [GTree a]
+    nodes node@(Node x ts) =
+      node : concat (map nodes ts)
+
+    nodeCount :: Int
+    nodeCount =
+      n where Node n _ = ncount gtree
+      --length (nodes gtree)
+
+
+ncount :: GTree a -> GTree Int
+ncount (Node _ []) =
+  Node 1 []
+ncount (Node _ ts) =
+  Node n ts'
+  where
+    ts' =
+      map ncount ts
+
+    n =
+      1 + foldr (\(Node x _) acc -> acc + x) 0 ts'
+
+
+depth :: GTree a -> Int
+depth (Node _ []) =
+  0
+depth (Node _ ts) =
+  1 + maximum [depth t | t <- ts]
+
+
+-- Fixed: https://ghc.haskell.org/trac/ghc/ticket/13106
+-- ƛ: mapbtree (\Node _ ts -> length ts) (Node 1 [Node 2 [Node 3 [], Node 4 []], Node 5 [Node 6 []], Node 7 []])
+-- ghc: panic! (the 'impossible' happened)
+--   (GHC version 8.0.2 for x86_64-apple-darwin):
+-- 	initTc: unsolved constraints
+--   WC {wc_insol =
+--         [W] mapbtree_aAty :: t_aAtx[tau:1] (CHoleCan: mapbtree)
+--         [W] mapbtree_aAua :: t_aAu9[tau:1] (CHoleCan: mapbtree)}
+
+-- Please report this as a GHC bug:  http://www.haskell.org/ghc/reportabug
+
+
+-- Ex. 11.2
+bestmoveRandom :: Int -> Grid -> Player -> IO Grid
+bestmoveRandom n g p = do
+  n <- randomRIO (0, length bestmoves)
+  return (bestmoves !! n)
+  where
+    bestmoves =
+      [g' | Node (g', p') _ <- ts', p' == best]
+
+    Node (_, best) ts' =
+      minimax . prune n . gametree g $ p
+
+
+mapgtree :: (a -> b) -> GTree a -> GTree b
+mapgtree f (Node x ts) =
+  Node (f x) [mapgtree f t | t <- ts]
+
+
+-- Ex. 11.3
+bestmoveQuickWin :: Int -> Grid -> Player -> Grid
+bestmoveQuickWin n g p =
+  grid . mingtree . map gtreeDepth . bestmoves n g $ p
+  where
+    grid :: GTree (Grid, Player, Int) -> Grid
+    grid (Node (g', _, _) _) =
+      g'
+
+    mingtree :: [GTree (Grid, Player, Int)] -> GTree (Grid, Player, Int)
+    mingtree xs =
+      foldr1 minDepth xs
+
+    minDepth :: GTree (Grid, Player, Int) -> GTree (Grid, Player, Int) -> GTree (Grid, Player, Int)
+    minDepth x@(Node (_, _, d) _) y@(Node (_, _, d') _)
+      | d <= d' =
+        x
+      | otherwise =
+        y
+
+
+gtreeDepth :: GTree (a, b) -> GTree (a, b, Int)
+gtreeDepth (Node (g, p) ts) =
+  Node (g, p, d) ts'
+  where
+    ts' =
+      [gtreeDepth t | t <- ts]
+
+    d =
+      if null ts' then 0 else 1 + maximum (map nodeDepth ts')
+
+    nodeDepth (Node (_, _, d') _) =
+      d'
+
+
+bestmoves :: Int -> Grid -> Player -> [GTree (Grid, Player)]
+bestmoves n g p =
+  [node | node@(Node (_, p') _) <- ts, p' == best]
+  where
+  Node (_, best) ts =
+    minimax . prune n . gametree g $ p
