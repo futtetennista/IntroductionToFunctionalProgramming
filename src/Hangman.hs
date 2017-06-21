069f4fd21c12d5bc00189ab994bf1c677d8bcf25
futtetennista <futtetennista@gmail.com>
2017-06-21 08:54:21 +0200

    Better read line

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -17,7 +17,7 @@ hangman = do
                play word guess mistakeCount c
          case state of
            Lost (p, n) ->
-             putState p (Just n) ; putStrLn ("You lost! The word was: " ++ word) ; hangman
+             do putState p (Just n) ; putStrLn ("You lost! The word was: " ++ word) ; hangman
 
            Won ->
              do putState word Nothing ; putStrLn "Got it!" ; hangman
@@ -30,20 +30,35 @@ hangman = do
 
 
 getWord :: IO String
-getWord =
-  do putStr "Enter a word: " ; sgetLine'
+getWord = do
+  putStr "Enter a word: " ; sgetLine'
   where
-    sgetChar = do
-      hSetEcho stdin False
-      c <- getChar
-      hSetEcho stdin True
-      return c
+    sgetChar =
+      do hSetEcho stdin False ; c <- getChar ; hSetEcho stdin True ; return c
 
-    sgetLine' = do
-      c <- sgetChar
-      if c == '\n'
-      then do putChar '\n' ; return []
-      else do putChar '*' ; cs <- sgetLine' ; return (c:cs)
+    -- sgetLine' = do
+    --   c <- sgetChar
+    --   if c == '\n' then do putChar '\n' ; return [] else do putChar '*' ; cs <- sgetLine' ; return (c:cs)
+
+    sgetLine' :: IO String
+    sgetLine' =
+      getLineHelp []
+      where
+        getLineHelp xs = do
+          c <- sgetChar
+          case c of
+            '\DEL' ->
+              do putStr "\b\ESC[0K" ; getLineHelp (safeTail xs)
+            '\n' ->
+              do putChar '\n' ; return (reverse xs)
+            _ ->
+              do putChar '*' ; getLineHelp (c:xs)
+
+        safeTail [] =
+          []
+        safeTail (_:xs) =
+          xs
+
 
     -- sgetLine input = do
     --   c <- sgetChar
