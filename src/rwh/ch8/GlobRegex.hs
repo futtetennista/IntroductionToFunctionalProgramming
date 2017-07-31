bf4846b07ec9f191dd7a23075cf0962df64f45c4
futtetennista <futtetennista@gmail.com>
2017-07-31 13:26:17 +0200

    Implement native glob patterns matcher

--- src/rwh/ch8/GlobRegex.hs
+++ src/rwh/ch8/GlobRegex.hs
@@ -12,6 +12,7 @@ import Data.Bits ((.|.))
 import Control.Monad.State (StateT, put, runStateT)
 import Control.Monad.Except
 import Control.Arrow (first)
+import Data.Char (isUpper, toLower, toUpper)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.Either as E (either)
@@ -52,7 +53,7 @@ globToRegex' cs
   | B.null cs =
     return ""
   | B.take 2 cs == "**" =
-    do put True ; globToRegex' cs'
+    put True >> globToRegex' cs'
   | headEqualTo '*' cs =
     (".*" <>) <$> globToRegex' cs'
   | headEqualTo '?' cs =
@@ -60,7 +61,8 @@ globToRegex' cs
   | B.take 2 cs == ("[!") =
     let cs' =
           B.drop 2 cs
-    in (("[^" <>) . (B.head cs' `B.cons`)) <$> charClass (B.tail cs')
+    in
+      (("[^" <>) . (B.head cs' `B.cons`)) <$> charClass (B.tail cs')
   | headEqualTo '[' cs && not (B.null cs') =
     (('[' `C8.cons`) . (B.head cs' `B.cons`)) <$> charClass (B.tail cs')
   | headEqualTo '[' cs && B.null cs' =
@@ -96,9 +98,9 @@ escape c =
 
 
 -- "(?i)foo" should be a valid POSIX regex makes the whole thing blow up with: `*** Exception: user error (Text.Regex.Posix.String died: (ReturnCode 13,"repetition-operator operand invalid"))` ?!
-matchesGlob :: FilePath -> B.ByteString -> Either ErrorMsg Bool
-matchesGlob fileName pat =
-  E.either Left (Right . fst . first (fileName =~)) (globToRegex pat)
+matchesGlobRegex :: FilePath -> B.ByteString -> CaseSensitive -> Either ErrorMsg (Bool, Recursive)
+matchesGlobRegex fileName pat _ =
+  E.either Left (Right . first (fileName =~)) (globToRegex pat)
 
 
 matchesGlob' :: FilePath -> B.ByteString -> CaseSensitive
@@ -109,12 +111,12 @@ matchesGlob' fileName pat csFlg =
       return (Left err)
 
     Right (regex, rec') ->
-      E.either returnErrorAsString (execRegex rec') =<< compileRegex regex
+      E.either failure (execRegex rec') =<< compileRegex regex
   where
     compileRegex =
       R.compile compOpt R.execBlank
 
-    returnErrorAsString =
+    failure =
       return . Left . show . snd
 
     compOpt =
@@ -131,3 +133,106 @@ matchesGlob' fileName pat csFlg =
             maybe False (const True)
       in
         return . E.either leftF rightF =<< R.regexec compRegex (C8.pack fileName)
+
+
+-- NATIVE MATCHER
+data Rule
+  = Str B.ByteString
+  | Anything
+  | AnySingleChar
+  | CharClass Bool B.ByteString
+  deriving Show
+
+
+type GlobPat =
+  [Rule]
+
+
+runToGlobPat :: B.ByteString -> Either ErrorMsg (GlobPat, Recursive)
+runToGlobPat xs =
+  runExcept (runStateT (toGlobPat xs) False)
+
+
+toGlobPat :: B.ByteString -> Result GlobPat
+toGlobPat xs
+  | C8.null xs =
+    return []
+  | C8.take 2 xs == "**" =
+    put True >> toGlobPat (C8.drop 2 xs)
+  | C8.head xs == '*' =
+    (Anything:) <$> toGlobPat (C8.tail xs)
+  | C8.head xs == '?' =
+    (AnySingleChar:) <$> toGlobPat (C8.tail xs)
+  | C8.take 2 xs == "[!" && (not . C8.null) (C8.tail xs) = do
+      (ccs, xs') <- toCharClass (C8.drop (2 :: Int) xs)
+      (CharClass False ccs:) <$> toGlobPat xs'
+  | C8.head xs == '[' = do
+      (ccs, xs') <- toCharClass (C8.tail xs)
+      (CharClass True ccs:) <$> toGlobPat xs'
+  | otherwise =
+    do (str, xs') <- toChar xs ; (Str str:) <$> toGlobPat xs'
+
+
+toCharClass :: B.ByteString -> Result (B.ByteString, B.ByteString)
+toCharClass xs
+  | C8.head xs == ']' =
+    throwError "unterminated character class"
+  | otherwise =
+    toCharClassHelp (B.empty, xs)
+  where
+    toCharClassHelp (xs', ys)
+      | C8.null ys =
+        throwError "unterminated character class"
+      | C8.head ys == ']' =
+        return (xs', C8.tail ys)
+      | otherwise =
+        toCharClassHelp (xs' `C8.snoc` C8.head ys, C8.tail ys)
+
+
+toChar :: B.ByteString -> Result (B.ByteString, B.ByteString)
+toChar xs =
+  return $ toCharHelp (B.empty, xs)
+  where
+    toCharHelp (ys, zs)
+      | C8.null zs =
+        (ys, zs)
+      | isPatternChar (C8.head zs) =
+        (ys, zs)
+      | otherwise =
+        toCharHelp (ys `C8.snoc` C8.head zs, C8.tail zs)
+
+    isPatternChar :: Char -> Bool
+    isPatternChar =
+      (`elem` ("[*?" :: String))
+
+
+matchesGlobNative :: FilePath -> B.ByteString -> CaseSensitive -> Either ErrorMsg (Bool, Recursive)
+matchesGlobNative [] _ _ =
+  Left "Empty file path"
+matchesGlobNative xs pat csFlg =
+  -- I didn't think about this before: even if `runToGlobPat` fails, the pattern matching will *not* fail because of how >>= works. Interesting.
+  do (pat', rec') <- runToGlobPat pat ; return $ (C8.pack xs `matches` pat', rec')
+  where
+    matches xs' [] =
+      C8.null xs'
+    matches _ (Anything:_) =
+      True
+    matches xs' (AnySingleChar:rs) =
+      (not . C8.null) xs' && matches (C8.tail xs') rs
+    matches xs' (Str str:rs) =
+      C8.take (C8.length str) xs' `caseAwareEq` str && matches (C8.drop (C8.length str) xs') rs
+    matches xs' (CharClass flg bs:rs) =
+      matchesCharClass (C8.head xs') bs && matches (C8.tail xs') rs
+      where
+        matchesCharClass c =
+          let f = if flg then id else not
+          in f . C8.any (==c) . C8.map (\x -> if isUpper x then toUpper x else toLower x)
+
+    caseAwareEq zs ys =
+      if csFlg then zs == ys else C8.map toLower zs == C8.map toLower ys
+
+
+
+matchesGlob :: FilePath -> B.ByteString -> CaseSensitive -> Either ErrorMsg (Bool, Recursive)
+matchesGlob =
+  matchesGlobNative
