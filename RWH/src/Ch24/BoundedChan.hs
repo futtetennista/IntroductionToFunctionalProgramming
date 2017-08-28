94831461f867006501cea9d919e7ae5dbdb76c61
futtetennista <futtetennista@gmail.com>
2017-08-28 21:46:44 +0200

    Improved implementation of BoundedChan
    Still not bug-free: it seems like when the last thread writes and exists after the main thread is blocked waiting for smth to be written in the channel, the msg written in the channel is for some reason not received from the main thread - that is the channle is empty AND no other threads hold a reference to the channel - and the program fails with an exception.

--- RWH/src/Ch24/BoundedChan.hs
+++ RWH/src/Ch24/BoundedChan.hs
@@ -27,35 +27,58 @@ newBoundedChan n = do
 
 writeBoundedChan :: BoundedChan a -> a -> IO ()
 writeBoundedChan (BoundedChan mvar) msg = do
-  ((counter, _), ch) <- readMVar mvar
-  n <- modifyMVar counter $
-    \x -> do writeChan ch msg ; return (x - 1, x - 1)
-  modifyMVar_ mvar $ \(st, ch) ->
-   if n == 0
-   then do x <- newEmptyMVar ; return (first (const x) st, ch)
-   else return (st, ch)
+  (counter, ch) <- modifyMVar mvar $
+    \bch@((counter, limit), ch) -> do
+      mMVar <- tryTakeMVar counter
+      case mMVar of
+        Nothing ->
+          return (bch, (counter, ch))
+        Just x
+          | x == 0 -> do
+              empty <- newEmptyMVar
+              return (((empty, limit), ch), (empty, ch))
+          | otherwise -> do
+              putMVar counter (x - 1)
+              return (bch, (counter, ch))
+  -- will block until counter is not empty
+  withMVar counter $ const (writeChan ch msg)
 
 
 readBoundedChan :: BoundedChan a -> IO a
 readBoundedChan (BoundedChan mvar) = do
-  ((counter, n), ch) <- readMVar mvar
-  flg <- isEmptyMVar counter
-  if flg
-    then putMVar counter 1
-    else modifyMVar_ counter (\x -> return (if x < n then x + 1 else x))
+  ch <- modifyMVar mvar $
+    \bch@((counter, limit), ch) -> do
+      mMVar <- tryTakeMVar counter
+      case mMVar of
+        Nothing ->
+          putMVar counter 1
+        Just x ->
+          putMVar counter $ max limit (x + 1)
+      return (bch, ch)
+  -- will block until there's something to read from ch
   readChan ch
 
 
+{-
+Wrote 'foo'
+Wrote 'bar'
+Read:foo
+Read:bar
+Wrote 'buzz'
+*** Exception: thread blocked indefinitely in an MVar operation
+-}
 main :: IO ()
 main = do
-  ch <- newBoundedChan 2 :: IO (BoundedChan [Char])
+  ch <- newBoundedChan 2
   sem <- newMVar ()
-  forkIO $ withMVar sem (\_ -> writeBoundedChan ch "foo" >> putStrLn "Wrote 'foo'")
-  forkIO $ withMVar sem (\_ -> writeBoundedChan ch "bar" >> putStrLn "Wrote 'bar'")
-  forkIO $ withMVar sem (\_ -> writeBoundedChan ch "buzz" >> putStrLn "Wrote 'buzz'")
-  threadDelay (1000*1000)
-  putStrLn . ("Read:"++) =<< readBoundedChan ch
-  putStrLn . ("Read:"++) =<< readBoundedChan ch
-  threadDelay (10^6)
-  putStrLn . ("Read:"++) =<< readBoundedChan ch
-  return ()
+  forkIO $ writeBoundedChan ch "foo" >> withMVar sem (\_ -> putStrLn "Wrote 'foo'")
+  forkIO $ writeBoundedChan ch "bar" >> withMVar sem (\_ -> putStrLn "Wrote 'bar'")
+  forkIO $ writeBoundedChan ch "buzz" >> withMVar sem (\_ -> putStrLn "Wrote 'buzz'")
+  --threadDelay (10^3 * 500)
+  xs <- readBoundedChan ch
+  withMVar sem (\_ -> putStrLn $ "Read:" ++ xs)
+  xs <- readBoundedChan ch
+  withMVar sem (\_ -> putStrLn $ "Read:" ++ xs)
+--  threadDelay (10^6)
+  xs <- readBoundedChan ch
+  putStrLn $ "Read:" ++ xs
